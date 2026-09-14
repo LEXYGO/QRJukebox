153bb37cd7c8683044bdf51d69a6932662d3e598
Lenny <l.git@familie-hanss.de>
2026-09-14 20:45:04 +0200

    Bump version to 1.0.11 and enable Android release optimizations (minification and resource shrinking)

--- lib/main.dart
+++ lib/main.dart
@@ -1128,7 +1128,7 @@ class _SettingsPageState extends State<SettingsPage> {
                       ),
                 ),
                 const Text(
-                  'Version 1.0.10',
+                  'Version 1.0.11',
                   style: TextStyle(color: Colors.grey),
                 ),
                 const Text(
